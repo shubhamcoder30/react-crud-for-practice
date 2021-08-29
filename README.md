e388270a3f0f7ca7928038f7d55c74017ff7b867
shubhamcoder30 <shubhams.48singh@gmail.com>
2021-08-29 22:15:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-au8c12
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-au8c12)
---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react'
+import AddUserForm from './forms/AddUserForm'
+import EditUserForm from './forms/EditUserForm'
+import UserTable from './tables/UserTable'
+
+const App = () => {
+	// Data
+
+	const initialFormState = { id: null, name: '', username: '' }
+
+	// Setting state
+	const [ users, setUsers ] = useState([])
+	const [ currentUser, setCurrentUser ] = useState(initialFormState)
+	const [ editing, setEditing ] = useState(false)
+
+	// CRUD operations
+	const addUser = user => {
+		user.id = users.length + 1
+		setUsers([ ...users, user ])
+	}
+
+	const deleteUser = id => {
+		setEditing(false)
+
+		setUsers(users.filter(user => user.id !== id))
+	}
+
+	const updateUser = (id, updatedUser) => {
+		setEditing(false)
+
+		setUsers(users.map(user => (user.id === id ? updatedUser : user)))
+	}
+
+	const editRow = user => {
+		setEditing(true)
+
+		setCurrentUser({ id: user.id, name: user.name, username: user.username })
+	}
+
+	return (
+		<div className="container">
+			<h1>CRUD App with Hooks</h1>
+			<div className="flex-row">
+				<div className="flex-large">
+					{editing ? (
+						<>
+							<h2>Edit user</h2>
+							<EditUserForm
+								editing={editing}
+								setEditing={setEditing}
+								currentUser={currentUser}
+								updateUser={updateUser}
+							/>
+						</>
+					) : (
+						<>
+							<h2>Add user</h2>
+							<AddUserForm addUser={addUser} />
+						</>
+					)}
+				</div>
+				<div className="flex-large">
+					<h2>View users</h2>
+					<UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default App
